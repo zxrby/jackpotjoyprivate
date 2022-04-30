722272ad33a07ec2c97102276586ade5c2e4483a
zxrby <79419506+zxrby@users.noreply.github.com>
2022-04-30 21:02:05 +0200

    Update jackpotjoy.lua

--- jackpotjoy.lua
+++ jackpotjoy.lua
@@ -8,6 +8,7 @@ local main = Instance.new("Frame")
 local FastOpen = Instance.new("TextButton")
 local TextLabel = Instance.new("TextLabel")
 local addall = Instance.new("TextButton")
+local _10t = Instance.new("TextButton")
 
 --Properties:
 
@@ -18,9 +19,9 @@ main.Name = "main"
 main.Parent = ScreenGui
 main.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 main.Position = UDim2.new(0.123997569, 0, 0.206134975, 0)
-main.Size = UDim2.new(0, 178, 0, 112)
+main.Size = UDim2.new(0, 182, 0, 122)
 main.Active = true
-main.Draggable = true
+main.Draggable = true 
 
 FastOpen.Name = "Fast Open "
 FastOpen.Parent = main
@@ -45,7 +46,6 @@ FastOpen.MouseButton1Click:Connect(function()
 	end
 end)
 
-
 TextLabel.Parent = main
 TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.BackgroundTransparency = 1.000
@@ -68,16 +68,20 @@ addall.TextColor3 = Color3.fromRGB(0, 0, 0)
 addall.TextSize = 14.000
 addall.MouseButton1Click:Connect(function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/zxrby/jackpotjoy/main/k.lua"))()
-	print("addall loadet succsesfully")
 end)
 
-while true do 
-
-wait(5)
-
-print("+++++++++++++++++++++++++++++++++++++")
-print("+          ZXRBY PRIVATE SCRIPT               +")
-print("+ FAST OPENING CANT BE DISABLED     + ")
-print("+++++++++++++++++++++++++++++++++++++")
-
-end
+_10t.Name = "10t"
+_10t.Parent = main
+_10t.BackgroundColor3 = Color3.fromRGB(105, 150, 255)
+_10t.Position = UDim2.new(0.123593807, 0, 0.609509408, 0)
+_10t.Size = UDim2.new(0, 53, 0, 39)
+_10t.Font = Enum.Font.SourceSans
+_10t.Text = "add 10t"
+_10t.TextColor3 = Color3.fromRGB(0, 0, 0)
+_10t.TextSize = 14.000
+_10t.MouseButton1Click:Connect(function()
+	local string_1 = "Glitched Monster Mask";
+	local string_2 = "120000";
+	local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+	Target:FireServer(string_1, string_2);
+end)
